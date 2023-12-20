c91f000df9512015e278535b9c6c85a7310c48b6
latifa lhafidi <lhafidilatifa59@gmail.com>
2023-12-20 10:40:08 +0100

    inplementer  la modale de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,25 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const main = document.getElementById("main");
+    const body = document.querySelector("body");
+    const btnFermenture = document.querySelector(".fermer");
+    modal.style.display = "block";
+    main.setAttribute('aria-hidden', 'true')
+    modal.setAttribute('aria-hidden', 'false')
+    body.setAttribute('class', 'no-scroll')
+    btnFermenture.focus()
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const main = document.getElementById("main");
+    const body = document.querySelector("body");
+    const btnOpen = document.querySelector("contact_button");
+    main.setAttribute('aria-hidden', 'false')
+    modal.setAttribute('aria-hidden', 'true')
     modal.style.display = "none";
+    body.removeAttribute('class', 'no-scroll');
+    btnOpen.focus();
+
+
 }
